43c25660305108ebf84dff743750e897905057b9
Saru1893 <sarathpandranki@gmail.com>
2026-09-23 23:31:04 +0530

    Initial project setup

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    cout << "Self-Healing CI/CD Engine Test Project"<<endl;
+    cout << "2 + 3 = " << add(2, 3) << endl;
+
+    return 0;
+}
