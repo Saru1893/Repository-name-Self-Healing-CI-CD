3f758b8bc58099d18a9937d2de1bf4cd1ac8c203
Saru1893 <sarathpandranki@gmail.com>
2026-09-26 00:41:20 +0530

    Test CI sanitizer failure

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include "math_utils.h"
 
@@ -7,6 +6,15 @@ using namespace std;
 int main() {
     cout << "Self-Healing CI/CD Engine Test Project\n";
     cout << "2 + 3 = " << add(2, 3) << '\n';
+
+    int* data = new int[3];
+
+    data[0] = 10;
+    data[1] = 20;
+    data[2] = 30;
+    data[3] = 40;  // Intentional buffer overflow
+
+    delete[] data;
 
     return 0;
 }
