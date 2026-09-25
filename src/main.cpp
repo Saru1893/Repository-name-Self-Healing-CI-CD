e4501fedddd18c17111c7f7d7dc40dc2cbf22c2d
Saru1893 <sarathpandranki@gmail.com>
2026-09-26 00:32:03 +0530

    Fix runtime failure

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,6 @@ using namespace std;
 
 int main() {
     cout << "Self-Healing CI/CD Engine Test Project\n";
-    int x = 10;
-    int y = 0;
-    cout << "Result = " << x / y << '\n';
     cout << "2 + 3 = " << add(2, 3) << '\n';
 
     return 0;
