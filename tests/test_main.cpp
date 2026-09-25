5035c4b9d6e87b85b95168b209b0cb7b67e086ed
Saru1893 <sarathpandranki@gmail.com>
2026-09-25 23:46:33 +0530

    Add automated C++ tests

---
+++ tests/test_main.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include "../src/math_utils.h"
+
+using namespace std;
+
+int main() {
+    if (add(2, 3) != 5) {
+        cerr << "Test failed: add(2, 3)\n";
+        return 1;
+    }
+
+    if (add(0, 0) != 0) {
+        cerr << "Test failed: add(0, 0)\n";
+        return 1;
+    }
+
+    if (add(-2, 3) != 1) {
+        cerr << "Test failed: add(-2, 3)\n";
+        return 1;
+    }
+
+    if (add(-4, -5) != -9) {
+        cerr << "Test failed: add(-4, -5)\n";
+        return 1;
+    }
+
+    cout << "All 4 tests passed!\n";
+
+    return 0;
+}
